69a5106078139c04f83a0b8bdc36a7b7393a3170
Georgi Tsekov <georgitce293@gmail.com>
2023-11-25 01:57:24 +0200

    Inserted information with some mocked data

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -23,7 +23,6 @@ CREATE TABLE hotel
     address             VARCHAR(64) UNIQUE    NOT NULL,
     city                VARCHAR(64)           NOT NULL,
     country             VARCHAR(100)          NOT NULL,
-    number_rooms        INTEGER               NOT NULL,
     rate                DECIMAL               NOT NULL, --constraint (from 1 to 5, ex. 3.5)
     is_pet_available    BOOLEAN,
     FOREIGN KEY (host_id) REFERENCES users (id) ON DELETE CASCADE
@@ -32,9 +31,7 @@ CREATE TABLE room_size_type
 (
     id                 BIGSERIAL PRIMARY KEY NOT NULL,
     room_type          VARCHAR(15) NOT NULL, --constraint (one, double, triple, apartment, presidential)
-    room_capacity      INTEGER,              --constraint(1-6)
-    num_children       INTEGER     NOT NULL,
-    num_adults         INTEGER     NOT NULL
+    room_capacity      INTEGER              --constraint(1-4)
 );
 
 CREATE TABLE room
@@ -44,6 +41,8 @@ CREATE TABLE room
     room_price          DECIMAL               NOT NULL,
     room_type           BIGINT                NOT NULL, --FK
     room_description    VARCHAR(500),
+    num_children        INTEGER               NOT NULL,
+    num_adults          INTEGER               NOT NULL,
     FOREIGN KEY (hotel_id) REFERENCES hotel (id) ON DELETE CASCADE,
     FOREIGN KEY (room_type) REFERENCES room_size_type (id) ON DELETE CASCADE
 );
@@ -60,18 +59,20 @@ CREATE TABLE occupancy
 (
     id                 BIGSERIAL PRIMARY KEY NOT NULL,
     room_id            BIGINT                NOT NULL,
-    from_date          TIMESTAMP             NOT NULL DEFAULT CURRENT_DATE,
-    to_date            TIMESTAMP             NOT NULL DEFAULT CURRENT_DATE + INTERVAL '1 DAY', --constraint (to_date after from_date - trqbwa pone 1 noshtuvka da ima)
+    check_in           DATE                  NOT NULL DEFAULT CURRENT_DATE,
+    check_out          DATE                  NOT NULL DEFAULT CURRENT_DATE + INTERVAL '1 DAY', --constraint (to_date after from_date - trqbwa pone 1 noshtuvka da ima)
+    created_at         TIMESTAMP             NOT NULL DEFAULT CURRENT_DATE,
+    deleted_at         TIMESTAMP,
     FOREIGN KEY (room_id) REFERENCES room (id) ON DELETE CASCADE
 );
 
 CREATE TABLE booking
 (
     id                 BIGSERIAL PRIMARY KEY NOT NULL,
-    room_id            BIGINT                NOT NULL,
+    occupancy_id       BIGINT                NOT NULL,
     user_id            BIGINT                NOT NULL,
     sum_price          DECIMAL               NOT NULL, --CALCULATE DATES FROM OCCUPANCY TABLE
-    FOREIGN KEY (room_id) REFERENCES room (id) ON DELETE CASCADE,
+    FOREIGN KEY (occupancy_id) REFERENCES occupancy (id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
--- src/main/resources/db/migration/V2__add_table_constraints.sql
+++ src/main/resources/db/migration/V2__add_table_constraints.sql
@@ -1,14 +1,32 @@
 ALTER TABLE users
 ADD CONSTRAINT check_user_type CHECK (user_type IN ('admin', 'user'));
 
+ALTER TABLE users
+ADD CONSTRAINT check_unique_email UNIQUE (email);
+
 ALTER TABLE hotel
 ADD CONSTRAINT check_hotel_rate CHECK (rate >= 1 AND rate <= 5);
 
+ALTER TABLE hotel
+ADD CONSTRAINT check_hotel_address UNIQUE (address);
+
 ALTER TABLE room_size_type
-ADD CONSTRAINT check_room_capacity CHECK (room_capacity >= 1 AND room_capacity <= 6);
+ADD CONSTRAINT check_room_capacity CHECK (room_capacity >= 1 AND room_capacity <= 4);
 
 ALTER TABLE room_size_type
 ADD CONSTRAINT check_room_type CHECK (room_type IN ('single', 'double', 'triple', 'apartment', 'presidential'));
 
+/* check that on the service side
+ALTER TABLE room
+ADD CONSTRAINT check_capacity CHECK ((SELECT room_capacity FROM room_size_type rst JOIN room r ON rst.id = r.room_type) >= (num_adults + num_children));
+*/
+CREATE EXTENSION btree_gist;
 ALTER TABLE occupancy
-ADD CONSTRAINT check_dates CHECK (to_date >= from_date + INTERVAL '1 DAY');
+ADD CONSTRAINT prevent_double_room_bookings EXCLUDE USING gist
+(
+  room_id WITH =,
+  daterange(check_in, check_out, '[)') WITH &&
+) WHERE (deleted_at IS NULL);
+
+ALTER TABLE occupancy
+ADD CONSTRAINT check_dates CHECK (check_out >= check_in + INTERVAL '1 DAY');
